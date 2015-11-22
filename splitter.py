9d0ed63bfc7cedfb41de10439fb1a6c33a8c33d7
zhaopeix <percy.peixi.zhao@gmail.com>
2015-11-22 16:29:14 -0500

    finished Q5

--- splitter.py
+++ splitter.py
@@ -6,6 +6,7 @@ def split_sentence(file_name):
     result = ""
     with open(file_name) as _file:
         for line in _file:
+            # use regex to substitute every matching pattern with a new line
             result += re.sub(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s',"\n", line)
     return result
 
