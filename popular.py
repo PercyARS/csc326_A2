b7c5297e4e2796b32debda0778d6bdaf6a4fd36a
zhaopeix <percy.peixi.zhao@gmail.com>
2015-11-22 01:13:36 -0500

    finished Q4

--- popular.py
+++ popular.py
@@ -4,12 +4,13 @@ from collections import defaultdict
 
 def find_popular(file_name):
     # use defaultdict to create a dictionary with 0 as value
-    d = defaultdict(lambda: 0, {})
+    d = defaultdict(lambda: 0)
     with open(file_name) as file:
         for line in file:
             for word in line.split():
                 d[word]+=1
-    # return the top 10 most popular words
+    # return the top 10 counts
     top_10_count = sorted(d.values())[::-1][:10]
+    # return the top 10 counted words, there might be more than 10
     top_10_counted_words = [key for key,value in d.items() if value in top_10_count][:10]
     print top_10_counted_words
